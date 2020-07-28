433cd24607ca260c6769866323dd6d82659e643d
pajsen9263 <chaspianstoltz@icloud.com>
2020-07-28 20:03:47 +0200

    Server.js
    Fixed the server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-/*var express = require("express");
+var express = require("express");
 var app = express();
 
 app.use(express.static("main"));
 
-app.get("/", (request, res) => { res.sendFile(__dirname + " ~/public/index.html"); });
+app.get("/", (request, res) => { res.sendFile(__dirname + " ~/index.html"); });
 
 app.get("/contact", (req, res) => { res.sendFile(__dirname + "~/public/contact.html"); });
 
 app.get("/about", (req, res) => { res.sendFile(__dirname + "~/public/about.html"); });
 
 app.get("/SeeME", (req, res) => { res.sendFile(__dirname + "~/public/software/SeeME.html"); });
-/*app.get("", (req, res) => { res.sendFile(__dirname + ""); });*/
+app.get("", (req, res) => { res.sendFile(__dirname + ""); });
 
-/*app.get("", (req, res) => { res.sendFile(__dirname + ""); });*/
+app.get("", (req, res) => { res.sendFile(__dirname + ""); });
 
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.sendFile(__dirname + "/views/404.html");
 });
 
