9908dede6e834e246bd0047882ec171ef42c3cd6
Glitch (freckle-delightful-wash) <none>
2020-09-22 10:53:25 +0000

    New update

---
+++ public/vue.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: "I'm known as Pajsen"
+  }
+})
+
+
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: false
+  }
+})
+
+
